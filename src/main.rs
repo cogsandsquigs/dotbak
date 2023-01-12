d2878ab38fb6f7eb7e724ce91f6c88bfcd094a05
Ian Pratt <ianjdpratt@gmail.com>
2023-01-11 19:50:27 -0600

    rm stuff

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ pub mod config;
 
 use clap::{Parser, Subcommand};
 use dialoguer::{theme::ColorfulTheme, Input};
-use git_url_parse::GitUrl;
 use std::{io, path::PathBuf};
 
 /// Manage and backup dotfiles with ease!
@@ -26,7 +25,7 @@ enum Action {
         /// The remote Git repository where the dotfiles are stored.
         /// TODO: Maybe git url type?
         #[arg(short, long)]
-        repository: Option<GitUrl>,
+        repository: Option<String>,
     },
 }
 
@@ -41,18 +40,10 @@ fn main() -> io::Result<()> {
             // Get the repository from the user if it is not provided already.
             let repository = match repository {
                 Some(r) => r,
-                None => GitUrl::parse(
-                    Input::with_theme(&ColorfulTheme::default())
-                        .with_prompt("Git repo where dotfiles are stored")
-                        .with_initial_text("https://github.com/username/repository")
-                        .validate_with(|url: &String| match GitUrl::parse(url) {
-                            Ok(..) => Ok(()),
-                            Err(..) => Err("Not a valid Git repo URL!"),
-                        })
-                        .interact_text()?
-                        .as_str(),
-                )
-                .expect("This should be correct as we check for invalid format beforehand"),
+                None => Input::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Git repo where dotfiles are stored")
+                    .with_initial_text("https://github.com/username/repository")
+                    .interact_text()?,
             };
 
             // TODO: business logic

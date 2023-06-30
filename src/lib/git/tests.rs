4be244b5b262387ed52b6185e6ac9186f5c11c56
Ian Pratt <ianjdpratt@gmail.com>
2023-06-30 14:00:13 -0500

    yay changes?

--- src/lib/git/tests.rs
+++ src/lib/git/tests.rs
@@ -145,3 +145,42 @@ fn test_clone_repo_path_nonexistent() {
     // Check if the .gitignore file exists.
     assert!(repo_dir.join(".gitignore").exists());
 }
+
+/// Test if we fail when cloning a repo into a repository that already exists.
+#[test]
+fn test_clone_repo_path_exists_already() {
+    // Create a temporary directory.
+    let tmp_dir = TempDir::new().unwrap();
+
+    // Get the path to the repo directory.
+    let repo_dir = tmp_dir.path();
+
+    // Initialize the repository.
+    let _repo: Result<gix::Repository, DotbakError> = Dotbak::init_repo(repo_dir);
+
+    // Check if the repository exists.
+    assert!(repo_dir.exists());
+
+    // Check if the .git folder exists.
+    assert!(repo_dir.join(".git").exists());
+
+    // Try to clone the repository again.
+    // THIS SHOULD PANIC
+    let result = Dotbak::clone_repo(
+        repo_dir,
+        Url::from_bytes("https://github.com/cogsandsquigs/dotbak".into()).unwrap(),
+    );
+
+    // Check if the result is an error.
+    assert!(result.is_err());
+
+    let err = result.unwrap_err();
+
+    println!("{}", err);
+
+    // Check that it is a git clone error.
+    assert!(match err {
+        DotbakError::Git(err) => matches!(*err, GitError::Clone(_)),
+        _ => false,
+    });
+}

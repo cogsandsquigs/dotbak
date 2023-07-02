04c19726f3a9712d1d0acaa4be1f9ba90c16fac4
Ian Pratt <ianjdpratt@gmail.com>
2023-07-01 21:07:09 -0400

    rm some comments [ci skip]

--- dotbak-core/src/errors/git.rs
+++ dotbak-core/src/errors/git.rs
@@ -2,8 +2,6 @@ use miette::Diagnostic;
 use snafu::Snafu;
 use std::path::PathBuf;
 
-// use super::DotbakError;
-
 /// Errors from doing Git operations.
 #[derive(Debug, Snafu, Diagnostic)]
 #[snafu(visibility(pub(crate)))]
@@ -36,39 +34,3 @@ pub enum GitError {
         source: git2::Error,
     },
 }
-
-// /* Convenience implementations for converting git errors into dotbak errors. */
-// /// Convert an init error into a `DotbakError`.
-// impl From<gix::init::Error> for DotbakError {
-//     fn from(err: gix::init::Error) -> Self {
-//         Self::Git(Box::new(GitError::Init(err)))
-//     }
-// }
-
-// /// Convert a clone error into a `DotbakError`.
-// impl From<gix::clone::Error> for DotbakError {
-//     fn from(err: gix::clone::Error) -> Self {
-//         Self::Git(Box::new(GitError::Clone(err)))
-//     }
-// }
-
-// /// Convert a fetch error into a `DotbakError`.
-// impl From<gix::clone::fetch::Error> for DotbakError {
-//     fn from(err: gix::clone::fetch::Error) -> Self {
-//         Self::Git(Box::new(GitError::Fetch(err)))
-//     }
-// }
-
-// /// Convert a worktree error into a `DotbakError`.
-// impl From<gix::clone::checkout::main_worktree::Error> for DotbakError {
-//     fn from(err: gix::clone::checkout::main_worktree::Error) -> Self {
-//         Self::Git(Box::new(GitError::Worktree(err)))
-//     }
-// }
-
-// /// Convert a find error into a `DotbakError`.
-// impl From<gix::remote::find::existing::Error> for DotbakError {
-//     fn from(err: gix::remote::find::existing::Error) -> Self {
-//         Self::Git(Box::new(GitError::Find(err)))
-//     }
-// }

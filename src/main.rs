9a54629ee058d8d346bf73add7b60913022633e1
Ian Pratt <ianjdpratt@gmail.com>
2023-07-12 09:09:19 -0500

    don't use log/pretty-env-logger rn

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 mod cli;
 
 use clap::Parser;
-use cli::Cli;
+use cli::{Action, Cli};
 use dotbak::Dotbak;
 use miette::Result;
 
 fn main() -> Result<()> {
-    let _cli = Cli::parse();
-    let _dotbak = Dotbak::init()?;
+    // Initialize pretty_env_logger with the custom variable DOTBAK_LOG (so that it doesn't conflict with other
+    // env_logger instances in other binaries).
+    pretty_env_logger::init_custom_env("DOTBAK_LOG");
+
+    let cli = Cli::parse();
+
+    // If the command is to deinitialize, then do so.
+    if matches!(cli.action, Action::Deinit) {
+        todo!();
+    }
+
+    // Otherwise, we continue along our happy path
+
+    // Initialize the `Dotbak` instance depending on what the user wants.
+    let _dotbak = match cli.action {
+        Action::Init { .. } => Dotbak::init()?,
+        _ => todo!(),
+    };
 
     Ok(())
 }

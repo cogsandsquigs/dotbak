b50f0207b50df4c056dabec73ccce44a2d287abb
Ian Pratt <ianjdpratt@gmail.com>
2023-07-12 21:30:27 -0500

    add more cmds for dotbak to prep for actual CLI

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,7 @@
+use std::path::PathBuf;
+
 use clap::Parser;
+use dotbak::{errors::Result, Dotbak};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -8,23 +11,65 @@ pub struct Cli {
     pub action: Action,
 }
 
+impl Cli {
+    /// Get the dotbak structure depending on the action.
+    pub fn get_dotbak(&self) -> Result<Dotbak> {
+        // Initialize the `Dotbak` instance depending on what the user wants.
+        match &self.action {
+            // If we are initializing, then just initialize.
+            Action::Init { repo_url: None } => Dotbak::init(),
+
+            // If we're provided a repository URL, then clone it.
+            Action::Clone { repo_url }
+            | Action::Init {
+                repo_url: Some(repo_url),
+            } => Dotbak::clone(repo_url),
+
+            // Otherwise, we just load the instance.
+            _ => Dotbak::load(),
+        }
+    }
+}
+
 #[derive(Parser)]
 pub enum Action {
-    /// Initializes a new instance of `dotbak` in the user's home directory.
+    /// Initializes a new instance of `dotbak` in your home directory (at `~/.dotbak`).
     Init {
-        /// The URL of the repository to clone.
-        #[clap(short, long)]
+        /// The URL of the repository to clone. This is essentially the same as 'dotbak clone <REPO_URL>'.
+        #[arg(short, long)]
         repo_url: Option<String>,
     },
 
-    /// Clones an instance of `dotbak` from the given URL to the user's home directory.
-    /// The user's home directory is assumed to be `<home>`.
+    /// Clones an instance of `dotbak` from the given URL to your home directory (at `~/.dotbak`).
     Clone {
         /// The URL of the repository to clone.
-        #[clap(short, long)]
         repo_url: String,
     },
 
-    /// Deinitializes an instance of `dotbak` in the user's home directory.
+    /// Adds files to the repository.
+    Add {
+        /// The paths to the files to add.
+        paths: Vec<PathBuf>,
+    },
+
+    /// Removes files from the repository.
+    Remove {
+        /// The paths to the files to remove.
+        paths: Vec<PathBuf>,
+    },
+
+    /// Pushes the repository to the remote.
+    Push,
+
+    /// Pulls the repository from the remote.
+    Pull,
+
+    /// Runs an arbitrary git command on the repository, as if you were in the repository directory.
+    Git {
+        /// The arguments to pass to `git`.
+        args: Vec<String>,
+    },
+
+    /// Deinitializes an instance of `dotbak` in your home directory.
     Deinit,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 mod cli;
 
 use clap::Parser;
-use cli::{Action, Cli};
-use dotbak::Dotbak;
+use cli::Cli;
 use miette::Result;
 
 fn main() -> Result<()> {
@@ -12,27 +11,8 @@ fn main() -> Result<()> {
 
     let cli = Cli::parse();
 
-    // If the command is to deinitialize, then do so.
-    if matches!(cli.action, Action::Deinit) {
-        todo!();
-    }
-
-    // Otherwise, we continue along our happy path
-
-    // Initialize the `Dotbak` instance depending on what the user wants.
-    let dotbak = match cli.action {
-        // If we are initializing, then just initialize.
-        Action::Init { repo_url: None } => Dotbak::init()?,
-
-        // If we're provided a repository URL, then clone it.
-        Action::Clone { repo_url }
-        | Action::Init {
-            repo_url: Some(repo_url),
-        } => Dotbak::clone(&repo_url)?,
-
-        // Otherwise, we just load the instance.
-        _ => Dotbak::load()?,
-    };
+    // Get the dotbak instance.
+    let dotbak = cli.get_dotbak()?;
 
     todo!();
 

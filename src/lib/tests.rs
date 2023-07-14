a2fc44f3ce7e2f1df282b37eea25527704d9f722
Ian Pratt <ianjdpratt@gmail.com>
2023-07-13 20:27:34 -0500

    add test for syncing + bump major ver!

--- src/lib/tests.rs
+++ src/lib/tests.rs
@@ -206,7 +206,7 @@ fn test_remove_files() {
 /// Test if we can deinitialize a `Dotbak` instance after adding files to it.
 #[test]
 fn test_delete_dotbak() {
-    let dir = TempDir::new().unwrap();
+    let dir: TempDir = TempDir::new().unwrap();
     let home_dir = dir.path().join("home");
     let config_file = dir.path().join("config.toml");
     let repo_dir = dir.path().join("repo");
@@ -242,4 +242,61 @@ fn test_delete_dotbak() {
     assert!(!config_file.exists());
     assert!(!repo_dir.exists());
     assert!(full_test_file_path.exists());
+}
+
+/// Test if we can synchronize the dotbak state. I.e., if we can add files that are not symlinked but in
+/// the repository, and replace files that are already there.
+#[test]
+fn test_sync_files() {
+    let dir: TempDir = TempDir::new().unwrap();
+    let home_dir = dir.path().join("home");
+    let config_file = dir.path().join("config.toml");
+    let repo_dir = dir.path().join("repo");
+    let mut dotbak = Dotbak::init_into_dirs(&home_dir, &config_file, &repo_dir).unwrap();
+
+    let test_file_1 = PathBuf::from("test.txt");
+    let test_file_2 = PathBuf::from("test2.txt");
+
+    dotbak.config.files.include = vec![test_file_1.clone(), test_file_2.clone()];
+
+    let full_test_file_path_1 = repo_dir.join(&test_file_1);
+    let full_test_file_path_2 = repo_dir.join(&test_file_2);
+
+    let expected_file_1 = home_dir.join("test.txt");
+    let expected_file_2 = home_dir.join("test2.txt");
+
+    // Create the home directory and the test file.
+    fs::create_dir_all(&home_dir).unwrap();
+
+    assert!(home_dir.exists());
+    assert!(!expected_file_1.exists());
+    assert!(!expected_file_2.exists());
+
+    fs::File::create(&full_test_file_path_1).unwrap();
+    fs::File::create(&full_test_file_path_2).unwrap();
+    fs::File::create(&expected_file_2).unwrap();
+
+    // Write content to the second file
+    fs::write(&full_test_file_path_2, "test").unwrap();
+
+    // Write content to the dummy file
+    fs::write(&expected_file_2, "dummy").unwrap();
+
+    assert!(full_test_file_path_1.exists());
+    assert!(full_test_file_path_2.exists());
+    assert!(!expected_file_1.exists());
+    assert!(expected_file_2.exists());
+    assert!(dotbak.config.files.include.contains(&test_file_1));
+    assert!(dotbak.config.files.include.contains(&test_file_2));
+    assert_eq!(fs::read_to_string(&expected_file_2).unwrap(), "dummy");
+
+    dotbak.sync().unwrap();
+
+    assert!(full_test_file_path_1.exists());
+    assert!(full_test_file_path_2.exists());
+    assert!(expected_file_1.exists());
+    assert!(expected_file_2.exists());
+    assert!(dotbak.config.files.include.contains(&test_file_1));
+    assert!(dotbak.config.files.include.contains(&test_file_2));
+    assert_eq!(fs::read_to_string(&expected_file_2).unwrap(), "test");
 }

62f625d38d54abc70d6c89492b141a3ec952f4c4
Ian Pratt <ianjdpratt@gmail.com>
2024-07-08 22:47:20 -0500

    Fix teensy tiny issue with mentioning unrelated project in panic errors

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn amend_panic_with_issue_msg() {
 
         println!();
 
-        println!("{}", console::style("This panic most likely should not have happened (unless your OS is very weird). However, Jundler is experimental and these types of things can happen.").yellow());
+        println!("{}", console::style("This panic most likely should not have happened (unless your OS is very weird). However, Dotbak is experimental and these types of things can happen.").yellow());
         println!("{}", console::style("If you feel that this panic was unjustified or unreasonable, submit an issue at https://github.com/cogsandsquigs/dotbak if you encounter any problems.").yellow());
         println!("{}", console::style("If you aren't sure what to do, submit an issue just in case. Better safe than sorry ;).").yellow());
     }));
